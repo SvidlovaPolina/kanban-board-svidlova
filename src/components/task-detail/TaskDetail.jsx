1d466301d06009dda37c171b4bab931c1d61b505
Polina <polina5523312@gmail.com>
2023-05-12 18:49:12 +0500

    changes TaskDetail + not found icon

--- src/components/task-detail/TaskDetail.jsx
+++ src/components/task-detail/TaskDetail.jsx
@@ -1,6 +1,7 @@
 import { useState } from 'react'
 import { useRouteMatch, Link } from 'react-router-dom'
 import { formatDate } from '../../utils'
+import notFoundIcon from '../../assets/not-found.svg'
 import FormAddDescription from '../forms/FormAddNewTask'
 import css from './TaskDetail.module.css'
 
@@ -15,28 +16,41 @@ const TaskDetail = (props) => {
 		setFormDescriptionVisible(!isFormDescriptionVisible)
 	}
 
-	return (
-		<div className={css.wrapper}>
-		<div className={css.close}>
-		<Link to='/' className={css.homeLink}>&#9587;</Link>
-		</div>
-		{task ? (
+	const renderTaskDetails = () => {
+		return (
 			<>
 				<div className={css.header}>
 					<h2 className={css.title}>{task.title}</h2>
 				</div>
-				<p className={css.createdAt}>{formatDate(task.created)}</p>
+				<p className={css.createdAt}>Created at: {formatDate(task.created)}</p>
 				<p>Description: {task.description || 'This task has no description'}</p>
 				<button className={css.addButton} onClick={AddDescriptionClick}>+ Add description</button>
-				
 				{isFormDescriptionVisible && (
 					<FormAddDescription addNewTask={addNewTask} setFormDescriptionVisible={setFormDescriptionVisible} />
 				)}
+
 			</>
-		) : (
-			<h2>Task with ID {taskId} not found</h2>
-		)}
-		</div>
+		)
+	}
+
+	const renderEmptyState = () => {
+		return (
+			<div className={css.emptyState}>
+				<h2>Task with ID <em>{taskId}</em> was not found</h2>
+				<img className={css.emptyStateIcon} src={notFoundIcon} alt='' />
+			</div>
+		)
+	}
+
+	return (
+		<>
+			<div className={css.wrapper}>
+			<div className={css.close}>
+				<Link to='/' className={css.homeLink}>&#9587;</Link>
+			</div>
+				{task ? renderTaskDetails() : renderEmptyState()}
+			</div>
+		</>
 	)
 }
 
